424c1bb38aeedf8291f2b6e146c6bdf9d342a09f
ritesh tharu <karukhusyalritesh@gmail.com>
2025-09-05 14:01:45 +0545

    updated something

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/react-project_one/',
+   base: '/Nepal-Diversity/',
   plugins: [
     react(),
     tailwindcss(),
